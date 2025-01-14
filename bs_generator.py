d28c1a465a4a4bd73a65de13e825a77c300a579d
Extauren <extauren@protonmail.com>
2025-01-15 00:08:26 +0100

    Add generate bsa air bs option

--- bs_generator.py
+++ bs_generator.py
@@ -2,9 +2,10 @@ import sys
 from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
 from data import Data
+from gui import gui
 
 def print_help() -> None:
-    print('python bs_generator.py "{structure_name}"')
+    print('python bs_generator.py "{structure_name} --bsa_air"')
 
 def html_to_pdf(html_content, output_path) -> None:
     HTML(string=html_content).write_pdf(output_path)
@@ -20,22 +21,27 @@ def generate_pfd(data: Data) -> None:
 def get_arguments() -> str:
     argv: list = sys.argv
     argv_nb: int = len(argv)
+    bsa_air: bool = False
 
-    if (argv_nb != 2):
+    if (argv_nb < 2):
         print(f"Wrong number of argument. Got {argv_nb - 1} but expected only 1")
         exit()
     if (argv[1] == '-h' or argv[1] == "--help"):
         print_help()
         exit()
-    return argv[1]
+    if argv_nb == 3 and argv[2] == "--bsa-air":
+        bsa_air = True
+    return argv[1], bsa_air
 
 def main():
     #generate word format
     structure: str = None
+    bsa_air: bool = False
 
-    structure = get_arguments()
-    data = Data(structure)
+    structure, bsa_air = get_arguments()
+    data = Data(structure, bsa_air)
     generate_pfd(data)
+    # gui()
 
 if __name__ == "__main__":
     main()
