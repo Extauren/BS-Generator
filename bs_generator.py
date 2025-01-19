296375aa379769c5fbb1927880c5e2d7b1ce3e90
Extauren <extauren@protonmail.com>
2025-01-19 19:21:04 +0100

    Add argparse package for arguments management

--- bs_generator.py
+++ bs_generator.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
 from data import Data
@@ -17,28 +17,20 @@ def generate_pfd(data: Data) -> None:
     html_to_pdf(html, "bulletin_de_souscription.pdf")
     print("PDF have been generated succesfully")
 
-def get_arguments() -> str:
-    argv: list = sys.argv
-    argv_nb: int = len(argv)
-    bsa_air: bool = False
-
-    if (argv_nb < 2):
-        print(f"Wrong number of argument. Got {argv_nb - 1} but expected only 1")
-        exit()
-    if (argv[1] == '-h' or argv[1] == "--help"):
-        print_help()
-        exit()
-    if argv_nb == 3 and argv[2] == "--bsa-air":
-        bsa_air = True
-    return argv[1], bsa_air
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('structure', type = str, help='structure name')
+    parser.add_argument('--docx', action='store_true', help='generate docx')
+    parser.add_argument('--bsa-air', action='store_true', help='generate bsa air bs')
+    args = parser.parse_args()
+    return args
 
 def main():
-    #generate word format
-    structure: str = None
-    bsa_air: bool = False
+    args = None
 
-    structure, bsa_air = get_arguments()
-    data = Data(structure, bsa_air)
+    args = get_arguments()
+    data = Data(args.structure, args.bsa_air)
     generate_pfd(data)
 
 if __name__ == "__main__":
