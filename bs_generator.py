2743a621628edb00cb88a52cdb0e77f218f12161
Extauren <extauren@protonmail.com>
2025-01-19 20:52:02 +0100

    Remove Spire.Doc packages

--- bs_generator.py
+++ bs_generator.py
@@ -1,18 +1,18 @@
 import argparse
 from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
-from spire.doc import *
+# from spire.doc import *
 from data import Data
 
-def convert_html_to_docx(html):
-    document = Document()
-    sec = document.AddSection()
-    paragraph = sec.AddParagraph()
+# def convert_html_to_docx(html):
+#     document = Document()
+#     sec = document.AddSection()
+#     paragraph = sec.AddParagraph()
 
-    paragraph.AppendHTML(html)
-    document.SaveToFile("test.docx", FileFormat.Docx2016)
-    document.Close()
-    print("Docx have been generated succesfully")
+#     paragraph.AppendHTML(html)
+#     document.SaveToFile("test.docx", FileFormat.Docx2016)
+#     document.Close()
+#     print("Docx have been generated succesfully")
 
 def html_to_pdf(html_content, output_path) -> None:
     HTML(string=html_content).write_pdf(output_path)
@@ -22,8 +22,8 @@ def generate_pfd(data: Data, docx: bool) -> None:
     template = env.get_template('template.jinja')
     html: str = template.render(data = data)
     
-    if docx is True:
-        convert_html_to_docx(html)
+    # if docx is True:
+    #     convert_html_to_docx(html)
     html_to_pdf(html, "bulletin_de_souscription.pdf")
     print("PDF have been generated succesfully")
 
