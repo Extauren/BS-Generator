0362792d3846c3fa8b8d836c46e093cf191bf38f
Extauren <extauren@protonmail.com>
2025-01-19 21:04:25 +0100

    Remove Spire.Doc package

--- bs_generator.py
+++ bs_generator.py
@@ -3,7 +3,6 @@ import argparse
 from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
 from data import Data
-from spire.doc import *
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QObject, Slot
@@ -19,16 +18,6 @@ class Console(QObject):
         data = Data(structure, False)
         generate_pfd(data, False, path.replace("file://", ""))
 
-# def convert_html_to_docx(html):
-#     document = Document()
-#     sec = document.AddSection()
-#     paragraph = sec.AddParagraph()
-
-#     paragraph.AppendHTML(html)
-#     document.SaveToFile("test.docx", FileFormat.Docx2016)
-#     document.Close()
-#     print("Docx have been generated succesfully")
-
 def html_to_pdf(html_content, output_path) -> None:
     HTML(string=html_content).write_pdf(output_path)
 
@@ -36,15 +25,13 @@ def generate_pfd(data: Data, docx: bool, path: str) -> None:
     env = Environment(loader = FileSystemLoader('templates'))
     template = env.get_template('template.jinja')
     html: str = template.render(data = data)
-    
-    # if docx is True:
-    #     convert_html_to_docx(html)
+
     html_to_pdf(html, "bulletin_de_souscription.pdf")
     print("PDF have been generated succesfully")
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument('structure', type = str, help='structure name')
     parser.add_argument('--docx', action='store_true', help='generate docx')
     parser.add_argument('--bsa-air', action='store_true', help='generate bsa air bs')
