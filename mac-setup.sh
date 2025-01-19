04e065cc33827929a7d9b86ad6f85cd19cc772ec
Extauren <extauren@protonmail.com>
2025-01-19 17:34:35 +0100

    Add pyside package installation

--- mac-setup.sh
+++ mac-setup.sh
@@ -3,5 +3,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 export LDFLAGS=-L/opt/homebrew/lib
 # check export or folder link
-brew install weasyprint python-tk
+brew install weasyprint pyside qt
 python3 -m pip install -r requirements.txt
