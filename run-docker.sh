72467c43677f10a56887900ab204c2a673f1f04e
Extauren <extauren@protonmail.com>
2025-01-21 00:35:10 +0100

    Fix dokcer image from local to ghcr

--- run-docker.sh
+++ run-docker.sh
@@ -5,7 +5,7 @@ output=$2
 docx=$3
 source .env >/dev/null
 
-docker run -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator bs python bs_generator.py -s $struc_name -n $output $docx
+docker run -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s $struc_name -n $output $docx
 docker cp bs_generator:/app/$2.pdf $FOLDER_OUTPUT
 if [ -n "$docx" ]; then
     docker cp bs_generator:/app/$2.docx $FOLDER_OUTPUT
