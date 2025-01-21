6e693181cb65169d397321e7c6ece20b5ae33003
Extauren <extauren@protonmail.com>
2025-01-21 23:11:07 +0100

    Fix space in path fo docker cp cmd and Remove build docker image arm

--- run-docker.sh
+++ run-docker.sh
@@ -7,8 +7,8 @@ source .env >/dev/null
 
 docker pull ghcr.io/extauren/bs_generator:latest >/dev/null
 docker run -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s $struc_name -n $output $docx
-docker cp bs_generator:/app/$2.pdf $FOLDER_OUTPUT
+docker cp bs_generator:/app/$2.pdf "$FOLDER_OUTPUT"
 if [ -n "$docx" ]; then
-    docker cp bs_generator:/app/$2.docx $FOLDER_OUTPUT
+    docker cp bs_generator:/app/$2.docx "$FOLDER_OUTPUT"
 fi
 docker rm bs_generator >/dev/null
