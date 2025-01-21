a08db0c4428688def11c52ddfbd12cf5b59728ca
Extauren <extauren@protonmail.com>
2025-01-21 23:40:42 +0100

    Remove output docker pull cmd to /dev/null

--- run-docker.sh
+++ run-docker.sh
@@ -5,10 +5,11 @@ output=$2
 docx=$3
 source .env >/dev/null
 
-docker pull ghcr.io/extauren/bs_generator:latest >/dev/null
+
+docker pull ghcr.io/extauren/bs_generator:latest
 docker run -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s "$struc_name" -n "$output" $docx
-docker cp bs_generator:/app/$2.pdf "$FOLDER_OUTPUT"
+docker cp bs_generator:/app/$output.pdf "$FOLDER_OUTPUT"
 if [ -n "$docx" ]; then
-    docker cp bs_generator:/app/$2.docx "$FOLDER_OUTPUT"
+    docker cp bs_generator:/app/$output.docx "$FOLDER_OUTPUT"
 fi
 docker rm bs_generator >/dev/null
