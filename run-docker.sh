cb4e3415cb4ae9eac1f9d3b7cd0451a6fd542d7c
Extauren <extauren@protonmail.com>
2025-01-21 23:50:25 +0100

    Add steps message

--- run-docker.sh
+++ run-docker.sh
@@ -3,10 +3,13 @@
 struc_name=$1
 output=$2
 docx=$3
+GR='\033[0;32m'
+NC='\033[0m'
 source .env >/dev/null
 
-
+echo -e "${GR}Check if new a version is available${NC}"
 docker pull ghcr.io/extauren/bs_generator:latest
+echo -e "${GR}PDF is generating, please wait...${NC}"
 docker run -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s "$struc_name" -n "$output" $docx
 docker cp bs_generator:/app/$output.pdf "$FOLDER_OUTPUT"
 if [ -n "$docx" ]; then
