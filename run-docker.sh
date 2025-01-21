52c1c9b4d3d40c024d101e4cc4f16eb6616bb651
Extauren <extauren@protonmail.com>
2025-01-21 23:53:55 +0100

    Fix git pull message

--- run-docker.sh
+++ run-docker.sh
@@ -7,7 +7,7 @@ GR='\033[0;32m'
 NC='\033[0m'
 source .env >/dev/null
 
-echo -e "${GR}Check if new a version is available${NC}"
+echo -e "${GR}Check if a new docker image is available${NC}"
 docker pull ghcr.io/extauren/bs_generator:latest
 echo -e "${GR}PDF is generating, please wait...${NC}"
 docker run -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s "$struc_name" -n "$output" $docx
