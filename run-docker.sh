bbcc71b91fb8de1c9c5714ba531d2c772b6f5883
Extauren <extauren@protonmail.com>
2025-03-20 16:35:28 +0100

    Fix rm container after copy file

--- run-docker.sh
+++ run-docker.sh
@@ -10,8 +10,9 @@ source .env >/dev/null
 echo -e "${GR}Check if a new docker image is available${NC}"
 docker pull ghcr.io/extauren/bs_generator:latest
 echo -e "${GR}PDF is generating, please wait...${NC}"
-docker run --rm  -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s "$struc_name" -n "$output" $docx
+docker run  -v "./pappers.json:/app/pappers.json:ro" --env-file .env --name bs_generator ghcr.io/extauren/bs_generator:latest python bs_generator.py -s "$struc_name" -n "$output" $docx
 docker cp bs_generator:/app/$output.pdf "$FOLDER_OUTPUT"
 if [ -n "$docx" ]; then
     docker cp bs_generator:/app/$output.docx "$FOLDER_OUTPUT"
 fi
+docker rm bs_generator
